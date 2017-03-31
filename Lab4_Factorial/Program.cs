1f8e129f1999d3744fc7f4ec4b8ae1d2b87287df
Allison G. Endres <aendres19@gmail.com>
2017-03-31 08:50:08 -0400

    Add Program to give factorial of number entered by the user

--- Lab4_Factorial/Program.cs
+++ Lab4_Factorial/Program.cs
@@ -8,8 +8,52 @@ namespace Lab4_Factorial
 {
     class Program
     {
+        public static bool Continue()
+        {
+            Console.WriteLine("Continue? (y/n)");
+            string input = Console.ReadLine();
+
+            input.ToLower();
+            bool run;
+
+            if(input == "n")
+            {
+                Console.WriteLine("Goodbye!");
+                run = false;
+            }
+            else if(input == "y")
+            {
+                run = true;
+            }
+            else
+            {
+                Console.WriteLine("Sorry, that input is not valid.");
+                run = Continue();
+            }
+            return run;
+        }
+
+
         static void Main(string[] args)
         {
+            //prompt user to enter an integer from 1 to 10
+            Console.WriteLine("Please Enter a number from 1 to 10: ");
+            long num = long.Parse(Console.ReadLine());
+            
+            bool run = true;
+
+            long factorial = 1;
+
+            for (long i = 1; i <= num; i ++)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine("The factorial of is :" + factorial);
+            run = Continue();
+            //display the factorial of the number entered
+            //prompt the user to continue
+
         }
     }
 }
